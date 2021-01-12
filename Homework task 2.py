719b696dc686f086ee93ccbbf9e291cfa16080bb
LipmannMike <Lypka.michael@gmail.com>
2021-01-12 19:21:41 +0300

    Corrected task 2 in lesson 1

--- Homework task 2.py
+++ Homework task 2.py
@@ -1,10 +1,7 @@
-user_number1 = input("Введите число\n>>> ")
-user_number2 = input("Введите еще одно число\n>>> ")
-user_number1 = int(user_number1)
-user_number2 = int(user_number2)
-user_numbers_summ = user_number1 + user_number2
-user_message1 = input("Введите сообщение\n>>> ")
-user_message2 = input("Введите еще одно сообщение\n>>> ")
+user_time_sec = int(input("Введите время в секундах\n>>>"))
 
-print(f"Вы вводили числа: '{user_number1}' '{user_number2}'\nСумма введенных чисел равна {user_numbers_summ}")
-print(f"Вы вводили следующие сообщения:\n'{user_message1}' \n'{user_message2}'")
+hours = user_time_sec // (60 * 60)
+minutes = (user_time_sec % (60 * 60)) // 60
+sec = (user_time_sec % (60 * 60)) % 60
+
+print(f"Точное время: {hours} : {minutes} : {sec}")
