2eb2614f1733f7ec3a4050e0c00d2c7050013172
LipmannMike <Lypka.michael@gmail.com>
2021-01-12 20:02:59 +0300

    Corrected task 2 in lesson 1

---
+++ Homework task 3.py
@@ -0,0 +1,11 @@
+n = input("Введите число: \n>>> ")
+summ_1 = n + n
+summ_2 = summ_1 + n
+n = int(n)
+summ_1 = int(summ_1)
+summ_2 = int(summ_2)
+result = (n + summ_1 + summ_2)
+print(n)
+print(summ_1)
+print(summ_2)
+print(f"Сумма чисел составляет: {result}")
