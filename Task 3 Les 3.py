e32cc935ba7a3481eb8bea111d00dc0d0b8d4c2b
LipmannMike <Lypka.michael@gmail.com>
2021-01-24 23:04:00 +0300

    Added tasks of my homework

--- Task 3 Les 3.py
+++ Task 3 Les 3.py
@@ -2,8 +2,14 @@
 возвращает сумму наибольших двух аргументов.
 '''
 def my_func(num_1, num_2, num_3):
-    sum_max = max(num_1, num_2, num_3)
-    my_func()
+    list_number = [num_1, num_2, num_3]
+    list_number.pop(list_number.index(min(num_1, num_2, num_3)))
+    print(list_number)
+    sum_max = sum(list_number)
     return sum_max
 
-print(my_func(1, 7, 3))
+
+print(my_func(2, 9, 3))
+
+
+
--- test.py
+++ test.py
@@ -6,19 +6,51 @@
 #
 # printname()
 
-'''Enumerate функция'''
-
-
-# my_list = ['efv', True, None, 435]
-# for value, ind in enumerate(my_list, 1):
+# '''Enumerate функция'''
+#
+# my_list = ('efv', True, None, 435)
+# for ind, value in enumerate(my_list, 1):
 #     print(ind, value)
 
-def my_pow(x):
-    return x ** 2 + 10
+# def my_pow(x):
+#     return x ** 2 + 10
 
-data = [1, 2, 3, 5, 7, 8]
+# data = [1, 2, 3, -5, 7, -8]
 
-new_list = []
-for i in data:
-    new_list.append(my_pow(i))
-print(new_list)
+# new_list = []
+# for i in data:
+#     new_list.append(my_pow(i))
+# print(new_list)
+
+# result = list(map(my_pow, data))
+# print(result)
+
+# def my_filter(x):
+#     return x < 0
+#
+# result = list(filter(my_filter, data))
+# print(result)
+
+# def my_name(name, *args):
+#     print(name, args)
+#
+#
+# my_name('Mike', 3, 8, 3, 'dfd', 3, )
+
+# def my_name(name, **kwargs):
+#     print(name, kwargs)
+#
+#
+# my_name('Mike', age=34, surname='Lypka')
+
+# names = ['Mike', 'Bob', 'Bill']
+# ages = [80, 50, 30]
+#
+# # for names, ages in zip(names, ages):
+# #     print(names, ages)
+#
+# print(dict(zip(names, ages)))
+
+data = [1, 2, 3, -5, 7, -8]
+result = list(filter(lambda x: x < 0, data))
+print(result)
