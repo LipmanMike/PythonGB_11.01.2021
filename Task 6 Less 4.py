39e08d8cd11e8985f43dedee2a1fe3bbc8c84f22
LipmannMike <Lypka.michael@gmail.com>
2021-01-31 12:45:40 +0300

    added homework

--- Task 6 Less 4.py
+++ Task 6 Less 4.py
@@ -1,10 +1,15 @@
-my_list = []
 
-while True:
-    num = int(input('введите целое число: '))
-    my_list.append((num))
-    if num == '':
-        break
+from itertools import count, cycle
+#
+# for i in count(int(input('Введите целое исло: '))):
+#     print(i)
+
+my_list = [1, 2, 3, 4, 5, 6, 77, 8, 443, 456]
+iter = cycle(my_list)
+stop = ''
+while stop != 'q':
+    print(next(iter), end='')
+    stop = input()
 
 
 
