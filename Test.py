826343fee9abc2f1cb134d9af41e6a8eabd7f53b
LipmannMike <Lypka.michael@gmail.com>
2021-02-01 00:03:08 +0300

    added homework

--- Test.py
+++ Test.py
@@ -7,3 +7,14 @@
 #         continue
 #     print('Введённая строка достаточной длины') # Разные другие действия здесь...
 
+generator = (param ** 3 for param in range(5))
+
+for el in generator:
+    print(el)
+
+# my_set = {el**3 for el in range(5, 10)}
+# print((my_set))
+
+# my_list = [i**3 for i in range(5, 10)]
+# print(my_list)
+# [125, 216, 343, 512, 729]
